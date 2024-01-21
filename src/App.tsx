c27f26d97bcc42667167daf001a5d747223ffddb
shota876 <sh.guliashvili1@gmail.com>
2024-01-21 09:26:10 -0800

    #6 page routes declared.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+
 function App() {
-  return <h1>Hi</h1>
+  return (
+  <>
+   <Navbar />
+   <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/store" element={<Store />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </Container>
+  </>
+  )
 }
 
 export default App
